1ce44899a25480ed69018bbab97387660ac07c25
Meeravali Shaik <meera496@Meeravalis-MacBook-Pro.local>
2022-10-09 21:14:22 +0530

    Add things

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // updated latest code and
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.get("http://www.google.com");
     }
 }
