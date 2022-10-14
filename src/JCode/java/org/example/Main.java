d088612ef88d6375973b6dfb2c127bfebd32f479
meera496 <meeravali092@gmail.com>
2022-10-14 20:03:43 +0530

    updated files in repostiry

--- src/JCode/java/org/example/Main.java
+++ src/JCode/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
     }
 
